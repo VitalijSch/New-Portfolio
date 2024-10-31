f964fe270bb14a1ff01bd064b6f1f24287703d61
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-10-31 20:01:31 +0100

    implement marquee and style slider

--- frontend/src/app/hero/hero.component.ts
+++ frontend/src/app/hero/hero.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { NgxFastMarqueeModule } from 'ngx-fast-marquee';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgxFastMarqueeModule, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  public sliderContent: string[] = [
+    'Available for remote work',
+    'Frontend Developer',
+    'Based in Lahr',
+    'open to work',
+  ];
 }
