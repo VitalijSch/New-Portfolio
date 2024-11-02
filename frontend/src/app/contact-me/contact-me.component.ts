3548db37ffc5b2bbea1c7f6188aa39bb5a62213d
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-11-03 00:51:48 +0100

    fixed slidershow on references component and create styling and html for contact-me component

--- frontend/src/app/contact-me/contact-me.component.ts
+++ frontend/src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.css'
 })
 export class ContactMeComponent {
+  public messageForm: FormGroup;
 
+  private fb: FormBuilder = inject(FormBuilder);
+
+  constructor() {
+    this.messageForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  public sendMessage(): void {
+
+  }
 }
--- frontend/src/app/references/references.component.ts
+++ frontend/src/app/references/references.component.ts
@@ -11,45 +11,49 @@ import { CommonModule } from '@angular/common';
 })
 export class ReferencesComponent {
   public currentIndex: number = 1;
-  public currentTranslate: string = 'translateX(0%)';
+  public previousTranslate: string = 'translateX(0)';
+  public currentTranslate: string = 'translateX(0)';
+  public nextTranslate: string = 'translateX(0)';
   public reviews: Review[] = [
     {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      from: 'lorem'
+      text: 'Du hast während deiner Umschulung gezeigt, wie gut du im Team arbeitest. Deine Ideen für die Frontend-Struktur haben unser Projekt effizienter gemacht, und du warst immer bereit, Feedback anzunehmen und Verbesserungen umzusetzen. Die Zusammenarbeit mit dir hat unser Team enorm vorangebracht!',
+      from: 'Bogdan G.'
     },
     {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      from: 'lorem'
+      text: 'Dein Einsatz in unserem Projekt war eine echte Bereicherung. Du hast nicht nur deine Aufgaben super gemeistert, sondern auch anderen geholfen, ihre Module optimal ins Gesamtsystem einzufügen. Deine Bereitschaft, Wissen zu teilen und zu unterstützen, hat die Teamarbeit echt gestärkt.',
+      from: 'Dennis B.'
     },
     {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      from: 'lorem'
+      text: 'Deine Hilfsbereitschaft und dein Engagement sind mir besonders aufgefallen. Du hast es geschafft, auch komplexe Themen verständlich zu erklären und anderen im Team zu helfen, wenn sie nicht weiterkamen. Deine offene Art und deine technischen Kenntnisse haben eine echte Teamkultur geschaffen.',
+      from: 'Benedikt M.'
     },
-  ]
+  ];
 
-  next() {
+  public next(): void {
     this.currentIndex = (this.currentIndex + 1) % this.reviews.length;
-    if (this.currentIndex === 0) {
-      this.currentTranslate = 'translate(684px)';
-    }
-    if (this.currentIndex === 1) {
-      this.currentTranslate = 'translate(0)';
-    }
-    if (this.currentIndex === 2) {
-      this.currentTranslate = 'translate(-684px)';
-    }
+    this.updateSlides();
   }
 
-  prev() {
+  public prev(): void {
     this.currentIndex = (this.currentIndex - 1 + this.reviews.length) % this.reviews.length;
+    this.updateSlides();
+  }
+
+  private updateSlides(): void {
     if (this.currentIndex === 0) {
-      this.currentTranslate = 'translate(684px)';
+      this.previousTranslate = 'translateX(684px)';
+      this.currentTranslate = 'translateX(684px)';
+      this.nextTranslate = 'translateX(-1368px)';
     }
     if (this.currentIndex === 1) {
-      this.currentTranslate = 'translate(0)';
+      this.previousTranslate = 'translateX(0)';
+      this.currentTranslate = 'translateX(0)';
+      this.nextTranslate = 'translateX(0)';
     }
     if (this.currentIndex === 2) {
-      this.currentTranslate = 'translate(-684px)';
+      this.previousTranslate = 'translateX(1368px)';
+      this.currentTranslate = 'translateX(-684px)';
+      this.nextTranslate = 'translateX(-684px)';
     }
   }
 }
